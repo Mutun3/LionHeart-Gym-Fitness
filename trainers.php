018f5777fa9ea8c7e5d214930d18b466608bf27c
Mutune Kagonia <114507847+Mutun3@users.noreply.github.com>
2024-09-27 06:29:29 +0300

    Add files via upload

---
+++ trainers.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LIONHEART</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts      -->
+
+<header class="header">
+
+    <a href="#" class="logo"> <span>LIONHEART</span>FITNESS </a>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="trainers.php">trainers</a>
+        <a href="pricing.php">pricing</a>
+        <a href="Register.php">Register</a>
+        <a href="blogs.php">blogs</a>
+    </nav>
+
+
+</header>
+
+<!-- header section ends     -->
+
+
+<!-- trainers section starts  -->
+
+<section class="trainers" id="trainers">
+
+    <h1 class="heading"> <span>expert trainers</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/trainer-1.jpg" alt="">
+            <div class="content">
+                <span>expert trainer</span>
+                <h3>Marcus Alonso</h3>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-pinterest"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/trainer-2.jpg" alt="">
+            <div class="content">
+                <span>expert trainer</span>
+                <h3>Elena Musk</h3>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-pinterest"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/trainer-3.jpg" alt="">
+            <div class="content">
+                <span>expert trainer</span>
+                <h3>Lane Hunning</h3>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-pinterest"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/trainer-4.jpg" alt="">
+            <div class="content">
+                <span>expert trainer</span>
+                <h3>Selena Audrey</h3>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-pinterest"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <a href="pricing.php" class="btn">get started</a>
+
+</section>
+
+<!-- trainers section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+    <div class="box">
+            <h3>quick links</h3>
+            <a class="links" href="home.php">home</a>
+            <a class="links" href="about.php">about</a>
+            <a class="links" href="pricing.php">pricing</a>
+            <a class="links" href="trainers.php">trainers</a>
+            <a class="links" href="Register.php">Register</a>
+            <a class="links" href="blogs.php">blogs</a>
+        </div>
+
+
+        <div class="box">
+            <h3>opening hours</h3>
+            <p> monday : <i> 6:00am - 9:30pm </i> </p>
+            <p> tuesday : <i> 6:00am - 9:30pm </i> </p>
+            <p> wednesday : <i> 6:00am - 9:30pm </i> </p>
+            <p> friday : <i> 6:00am - 9:30pm </i> </p>
+            <p> saturday : <i> 6:00am - 9:30pm </i> </p>
+            <p> sunday : <i> closed </i> </p>
+        </div>
+
+        <div class="box">
+            <h3>opening hours</h3>
+            <p> <i class="fas fa-phone"></i> 0114790453 </p>
+            <p> <i class="fas fa-phone"></i> 0114866154</p>
+            <p> <i class="fas fa-envelope"></i> SAMSIMBA@gmail.com </p>
+            <p> <i class="fas fa-map"></i> Nyeri, Kenya - 53185 </p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+                <a href="#" class="fab fa-pinterest"></a>
+            </div>
+        </div>
+
+        
+
+    </div>
+
+</section>
+
+<div class="credit"> created by <span>Mutune and Simba</span> | all rights reserved! </div>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
